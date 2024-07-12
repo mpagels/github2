bc97b6356db5d408bfc830b449af4902344f4212
mpagels <54703643+mpagels@users.noreply.github.com>
2024-07-12 14:21:26 +0200

    app greets now some people

--- src/main/java/de/mpagels/Main.java
+++ src/main/java/de/mpagels/Main.java
@@ -3,6 +3,7 @@ package de.mpagels;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello People");
         System.out.println("Hello world!");
     }
 }
